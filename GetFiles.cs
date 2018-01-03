5e9a1145734c49fd731c64657da7a5e81fdb7dce
And-Pop <35067179+And-Pop@users.noreply.github.com>
2018-01-03 16:48:18 +0200

    Add files via upload

---
+++ GetFiles.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Media;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Configuration;
+
+namespace GetFiles
+{
+    public partial class Recursion : Form
+    {
+        #region FIELDS
+        private List<string> configurationFile = new List<string>
+        {
+            "[lastFilterLocation]",
+            "[lastFileExtensionUsed]",
+            "[lastSelectionColor]",
+            "[lastSelectionHide]"
+        };
+        #endregion
+        public Recursion()
+        {
+            InitializeComponent();
+
+            BtnOpenFile.Enabled = false;
+            BtnDeleteFile.Enabled = false;
+
+            this.btnFilter.Click -= new System.EventHandler(this.BtnFilter_Click);
+            this.btnFilter.Click += new EventHandler(this.CbColored_CheckedChanged);
+            this.btnFilter.Click += new System.EventHandler(this.BtnFilter_Click);
+
+            // fill in the textbox Directory path and the last file extension used
+            tbDirectory.Text = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            tbFileType.Text = CfgLastUsed(configurationFile[1]);
+            cbColored.Checked = Convert.ToBoolean(CfgLastUsed(configurationFile[2]));
+
+            label3.Text = string.Empty;
+
+            CfgInitiate();
+
+            // Test ce mai faci?
+            label3.Text = string.Empty;
+            // Testing
+        }
+	}
+}
